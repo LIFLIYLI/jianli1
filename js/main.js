6ead6495d7da1ad003ba8727aa752810bbac9700
LIFLIYLI <806820876@qq.com>
2018-11-06 14:38:05 +0800

    添加轮播

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+//swiper 初始化
+var mySwiper = new Swiper ('.swiper-container', {
+// Optional parameters
+loop: true,
+
+// If we need pagination
+pagination: {
+el: '.swiper-pagination',
+},
+
+// Navigation arrows
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+
+})
+
 setTimeout(function(){
  kaishi.classList.remove('active')
 },500)
